f63ef47c0165cac954315ea9d28d29a2ea296ffc
Pa Khan <don@pa-khan.com>
2024-10-24 02:31:15 +0300

    DataBase AttemptDelay

--- configo.go
+++ configo.go
@@ -8,15 +8,16 @@ import (
 )
 
 type Database struct {
-	Type          string `yaml:"type" env-required:"true"`
-	Host          string `yaml:"host" env-required:"true"`
-	Port          int    `yaml:"port" env-required:"true"`
-	Name          string `yaml:"name" env-required:"true"`
-	User          string `yaml:"user" env-required:"true"`
-	Password      string `yaml:"password" env-required:"true"`
-	Schema        string `yaml:"schema" env-default:"public"`
-	MigrationPath string `yaml:"migrationPath" env-required:"true"`
-	MaxAttempts   int    `yaml:"maxAttempts" env-required:"true"`
+	Type          string        `yaml:"type" env-required:"true"`
+	Host          string        `yaml:"host" env-required:"true"`
+	Port          int           `yaml:"port" env-required:"true"`
+	Name          string        `yaml:"name" env-required:"true"`
+	User          string        `yaml:"user" env-required:"true"`
+	Password      string        `yaml:"password" env-required:"true"`
+	Schema        string        `yaml:"schema" env-default:"public"`
+	MigrationPath string        `yaml:"migrationPath" env-required:"true"`
+	MaxAttempts   int           `yaml:"maxAttempts" env-required:"true"`
+	AttemptDelay  time.Duration `yaml:"attemptDelay" env-required:"true"`
 }
 
 type Redis struct {
